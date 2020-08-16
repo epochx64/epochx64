29e98630dc75b0b1d4067d9a688197f45a52c4f4
epochx64 <diodsavart@protonmail.com>
2020-08-16 18:01:02 -0400

    forgot to add extra libraries

---
+++ src/include/mem.h
@@ -0,0 +1,21 @@
+#ifndef MEM_H
+#define MEM_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include "../kernel/defs.h"
+
+typedef uint8_t byte;
+
+namespace mem
+{
+    void set(byte *dst, byte val, uint64_t count);
+    void copy(byte *src, byte *dst, size_t count);
+    void zero(byte *dst, size_t count);
+    uint8_t *alloc(size_t count);
+    void dealloc(byte *log);
+}
+
+#endif
