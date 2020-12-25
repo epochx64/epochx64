d5c3b2b3785a082f45d518a72cbd66c789034703
epochx64 <diodsavart@protonmail.com>
2020-12-25 12:48:34 -0500

    fixed redefinition issue

--- src/kernel/typedef.h
+++ src/kernel/typedef.h
@@ -19,9 +19,7 @@ namespace interrupt
     #define PIC1		    0x20		/* IO base address for master PIC */
     #define PIC2		    0xA0		/* IO base address for slave PIC */
     #define PIC1_COMMAND	PIC1
-    #define PIC1_DATA	    (PIC1+1)
     #define PIC2_COMMAND	PIC2
-    #define PIC2_DATA	    (PIC2+1)
 
     #define PS2_COMMAND_PORT 0x64
 
