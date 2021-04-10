7a48c8421a342f3721980be6fb9495f301d08004
epochx64 <diodsavart@protonmail.com>
2021-04-09 20:38:30 -0400

    minor bug change

--- src/lib/asm/asm.s
+++ src/lib/asm/asm.s
@@ -1,3 +1,39 @@
+    %macro pushaq 0
+        push r15
+        push r14
+        push r13
+        push r12
+        push r11
+        push r10
+        push r9
+        push r8
+        push rbp
+        push rsi
+        push rdi
+        push rdx
+        push rcx
+        push rbx
+        push rax
+    %endmacro
+
+    %macro popaq 0
+        pop rax
+        pop rbx
+        pop rcx
+        pop rdx
+        pop rdi
+        pop rsi
+        pop rbp
+        pop r8
+        pop r9
+        pop r10
+        pop r11
+        pop r12
+        pop r13
+        pop r14
+        pop r15
+    %endmacro
+
     section .apbootstrapdata
 ; -------------------------------------------------
 ;               IDT and GDT Structures
@@ -334,6 +370,8 @@ nop:
 
     global inb
 inb:
+    push rdx
+
     xor rax, rax
     mov dx, di  ;  Port #
     in al, dx
@@ -343,6 +381,9 @@ inb:
 .1:
     jmp .2
 .2:
+
+    pop rdx
+
     ret
 
     global outb
@@ -356,14 +397,13 @@ outb:
 
     out dx, al
 
-    pop rax
-    pop rdx
-
     ; Perform a delay
     jmp .1
 .1:
     jmp .2
 .2:
-    ret
+
+    pop rax
+    pop rdx
 
     ret
